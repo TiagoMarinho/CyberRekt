9396d16d613c429d5aff811136519cb4468028d2
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-01 20:54:37 -0300

    remove unnecessary code

--- js/backup/readdatafromslot.js
+++ js/backup/readdatafromslot.js
@@ -1,17 +1,10 @@
 const fs = require('node:fs');
 
 const readDataFromSlot = async (guildId, slot) => {
-	const path = `./db/server save states/${guildId}`
-	const fullPath = `${path}/slot${slot}.json`
-
-	await fs.promises.mkdir(path, { recursive: true })
-
-	await fs.promises.access(fullPath, fs.constants.F_OK)
-
-	const file = await fs.promises.readFile(fullPath)
-
+	const dir = `./db/server save states/${guildId}`
+	const filePath = `${dir}/slot${slot}.json`
+	const file = await fs.promises.readFile(filePath)
 	const data = JSON.parse(file)
-
 	return data
 }
 
