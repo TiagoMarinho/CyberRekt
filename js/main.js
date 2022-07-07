dc0bd96e32a486fec335e035787b49af9481b32b
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-07 01:24:22 -0300

    make command fetching into a reusable function

--- js/main.js
+++ js/main.js
@@ -1,53 +1,45 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('../config.json');
+const fs = require('node:fs')
+const path = require('node:path')
+const { Client, Collection, Intents } = require('discord.js')
+const { token } = require('../config.json')
+const fetchCommandsByCategory = require('./helpers/fetchcommands')
 
 class DiscordBot {
-	
-	client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+	client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 	run () {
 		this.client.once('ready', () => {
 			console.log(`Ready!`)
 			this.client.user.setActivity(`channels disappear`, {type: `WATCHING`})
-
-			this.fetchCommands()
-			this.handleCommands()
-			this.listenToRateLimit()
 		});
 
 		this.client.login(token);
 	}
-	fetchCommands () {
-		this.client.commands = new Collection();
-		
-		const commandCategoryFolders = fs.readdirSync('./js/commands');
-		for (const categoryFolder of commandCategoryFolders) {
-			const commandFolders = fs.readdirSync(`./js/commands/${categoryFolder}`);
-			for (const commandFolder of commandFolders) {
-				const commandFiles = fs.readdirSync(`./js/commands/${categoryFolder}/${commandFolder}`).filter(file => file.endsWith('.js'));
-				for (const file of commandFiles) {
-					const filePath = path.resolve(__dirname, `commands/${categoryFolder}/${commandFolder}/${file}`)
-					const command = require(filePath);
-					this.client.commands.set(command.data.name, command);
-				}
+	loadCommands () {
+		this.client.commands = new Collection()
+
+		const commandsByCategory = fetchCommandsByCategory(`./js/commands`)
+
+		for (const categoryName of Object.keys(commandsByCategory)) {
+			for (const command of commandsByCategory[categoryName]) {
+				this.client.commands.set(command.data.name, command)
 			}
 		}
 	}
 	handleCommands () {
 		this.client.on('interactionCreate', async interaction => {
-			if (!interaction.isCommand()) return;
+			if (!interaction.isCommand()) return
 		
-			const command = this.client.commands.get(interaction.commandName);
+			const command = this.client.commands.get(interaction.commandName)
 		
-			if (!command) return;
+			if (!command) return
 		
 			try {
-				await command.execute(interaction);
+				await command.execute(interaction)
 			} catch (error) {
 				console.error(error);
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
 			}
 		});
 	}
@@ -60,3 +52,6 @@ class DiscordBot {
 
 const cyberRekt = new DiscordBot()
 cyberRekt.run()
+cyberRekt.loadCommands()
+cyberRekt.handleCommands()
+cyberRekt.listenToRateLimit()
