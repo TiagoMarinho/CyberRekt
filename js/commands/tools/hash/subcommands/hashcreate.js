b9cdb80a191a0aa2b2d4641c5246ad2ae473acf3
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-07 21:53:56 -0300

    improve formatting for hash response

--- js/commands/tools/hash/subcommands/hashcreate.js
+++ js/commands/tools/hash/subcommands/hashcreate.js
@@ -4,6 +4,9 @@ const createHash = interaction => {
 	const input = interaction.options.getString(`input`)
 	const salt = randomBytes(16).toString(`hex`)
 	const hash = scryptSync(input, salt, 64).toString(`hex`)
-	return interaction.reply(`\`\`\`${salt}:${hash}\`\`\``);
+
+	const informationString = `Salt in yellow, hash in green:`
+	const formattedHashCodeBlock = `\`\`\`properties\n${salt}:${hash}\`\`\``
+	return interaction.reply(`${informationString}\n${formattedHashCodeBlock}`);
 }
 module.exports = createHash
