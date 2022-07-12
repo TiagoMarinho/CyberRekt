6582db91fda9496df6e83526b2b21fa4310d3494
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-12 14:32:28 -0300

    fix `/backup load` failing if missing permissions

--- js/commands/tools/backup/helpers/loadserverstate.js
+++ js/commands/tools/backup/helpers/loadserverstate.js
@@ -9,6 +9,7 @@ const loadServerState = async (guild, slot) => {
 
 	for (const id of Object.keys(channels)) {
 		await guild.channels.edit(id, { name: channels[id] })
+			.catch(console.error) // OPTIMIZE: temporary workaround for trying to edit channels it doesn't have access to
 	}
 	for (const id of Object.keys(roles)) {
 		await guild.roles.edit(id, { name: roles[id] })
