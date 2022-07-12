5d2ed889ba7dc6eb5262161bf7a992b9e4b85ddb
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-12 14:31:43 -0300

    fix incorrect `/hash` command information

--- js/commands/tools/hash/subcommands/hashcreate.js
+++ js/commands/tools/hash/subcommands/hashcreate.js
@@ -1,12 +1,18 @@
 const { scryptSync, randomBytes } = require('crypto')
+const { MessageEmbed, Formatters } = require('discord.js')
 
 const createHash = interaction => {
 	const input = interaction.options.getString(`input`)
 	const salt = randomBytes(16).toString(`hex`)
 	const hash = scryptSync(input, salt, 64).toString(`hex`)
 
-	const informationString = `Salt in yellow, hash in green:`
 	const formattedHashCodeBlock = `\`\`\`properties\n${salt}:${hash}\`\`\``
-	return interaction.reply(`${informationString}\n${formattedHashCodeBlock}`);
+
+	const embed = new MessageEmbed()
+		.setColor('#A50A39')
+		.setTitle('Hash result')
+		.setDescription(formattedHashCodeBlock)
+
+	interaction.reply({embeds: [embed]})
 }
 module.exports = createHash
