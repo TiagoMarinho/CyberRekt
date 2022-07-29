7bd3ceed7971222bf52add183200e167af56ec0d
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-29 18:58:15 -0300

    add a line break

--- js/commands/tools/backup/helpers/listserverstates.js
+++ js/commands/tools/backup/helpers/listserverstates.js
@@ -3,7 +3,8 @@ const readDataFromSlot = require("./readdatafromslot")
 const listServerStates = async (guild) => {
 	const serverStateInfos = []
 	for (let slot = 1; slot < 10; ++slot) {
-		const serverState = await readDataFromSlot(guild.id, slot).catch(console.error)
+		const serverState = await readDataFromSlot(guild.id, slot)
+			.catch(console.error)
 		const serverStateInfo = serverState?.info
 		serverStateInfos.push(serverStateInfo)
 	}
