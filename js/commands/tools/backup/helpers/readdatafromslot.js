429abce88b7078c200c66e8cfc470b66e261fd08
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-29 18:56:40 -0300

    fix #7

--- js/commands/tools/backup/helpers/readdatafromslot.js
+++ js/commands/tools/backup/helpers/readdatafromslot.js
@@ -1,11 +1,21 @@
-const fs = require('node:fs');
+const fs = require('node:fs')
 
 const readDataFromSlot = async (guildId, slot) => {
 	const dir = `./db/guilds/${guildId}/backups`
 	const filePath = `${dir}/slot${slot}.json`
-	const file = await fs.promises.readFile(filePath)
-	const data = JSON.parse(file)
-	return data
+	return fs.promises.readFile(filePath)
+		.then(file => {
+			const data = JSON.parse(file)
+			return data
+		})
+		.catch(err => {
+			switch (err.code) {
+				case `ENOENT`:
+					return null
+				default:
+					throw err
+			}
+		})
 }
 
 module.exports = readDataFromSlot
