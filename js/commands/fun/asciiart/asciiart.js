5ccb906618747a0a82e3d2ee22f73c4c1e51bdf0
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-29 18:47:56 -0300

    add color selection to ascii art command

--- js/commands/fun/asciiart/asciiart.js
+++ js/commands/fun/asciiart/asciiart.js
@@ -16,12 +16,29 @@ module.exports = {
 				{ name: 'Roman', value: 'roman' },
 				{ name: 'ABC', value: 'abc' },
 				{ name: 'Thin', value: 'thin' },
+			))
+		.addStringOption(option => 
+			option.setName('color')
+			.setDescription(`Color of text, desktop only`)
+			.setChoices(
+				{ name: 'White', value: '00' },
+				{ name: 'Blue', value: '11' },
+				{ name: 'Red', value: '22' },
+				{ name: 'Green', value: '32' },
+				{ name: 'Yellow', value: '03' },
 			)),
 	async execute(interaction) {
 
 		const text = interaction.options.getString(`input`).toUpperCase()
 		const fontName = interaction.options.getString(`font`) || 'roman'
 		const font = require(`./fonts/${fontName}.json`)
+		const color = (interaction.options.getString(`color`) || `00`)
+		const colorID = color.split(``).map(s => parseInt(s))
+
+		const prefixes = [``, `# `, `- `, `+ `]
+		const langs = [``, `markdown`, `diff`, `fix`]
+		const prefix = prefixes[colorID[0]]
+		const lang = langs[colorID[1]]
 
 		const result = text
 			.split(``)
@@ -31,12 +48,12 @@ module.exports = {
 		const final = 
 			new Array(font.info.height)
 			.fill()
-			.map((_, index) => result.reduce(
+			.map((_, index) => prefix + result.reduce(
 				(accumulator, fontChar) => 
 				`${accumulator}${fontChar[index]}  `, ``
 			))
 			.join(`\n`)
 		
-		interaction.reply(`\`\`\`${final}\`\`\``)
+		interaction.reply(`\`\`\`${lang}\n${final}\n\`\`\``)
 	},
 };
