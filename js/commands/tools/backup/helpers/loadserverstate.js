3124689cd6536d1f51f8c46ab615b16868fb1d92
Tiago Marinho <tiagomeraki@gmail.com>
2022-07-29 19:01:14 -0300

    handle errors and show reasons to user

--- js/commands/tools/backup/helpers/loadserverstate.js
+++ js/commands/tools/backup/helpers/loadserverstate.js
@@ -1,20 +1,58 @@
+const { SlashCommandSubcommandBuilder } = require("@discordjs/builders")
 const readDataFromSlot = require("./readdatafromslot")
 
+const loadServerState = async (client, guild, slot) => {
 
-const loadServerState = async (guild, slot) => {
 	const saveState = await readDataFromSlot(guild.id, slot)
+
+	if (saveState === null)
+		return null
 
 	const { title } = saveState.info
 	const { channels, roles } = saveState.data
 
+	const ownRole = guild.roles.botRoleFor(client.user)
+
+	const channelLoadStatuses = []
+	const roleLoadStatuses = []
+
 	for (const id of Object.keys(channels)) {
-		await guild.channels.edit(id, { name: channels[id] })
-			.catch(console.error) // OPTIMIZE: temporary workaround for trying to edit channels it doesn't have access to
+		const channel = guild.channels.cache.get(id)
+		const status = {
+			previousName: channel?.name,
+			newName: channels[id],
+			id: id,
+			error: null
+		}
+		await channel?.edit({ name: channels[id] })
+			.catch(err => {
+				status.error = err
+			})
+
+		channelLoadStatuses.push(status)
 	}
+	
 	for (const id of Object.keys(roles)) {
-		await guild.roles.edit(id, { name: roles[id] })
-			.catch(console.error) // OPTIMIZE: temporary workaround for trying to edit its own role
+		if (id === ownRole?.id)
+			continue
+		
+		const role = guild.roles.cache.get(id)
+		const status = {
+			previousName: role?.name,
+			newName: roles[id],
+			id: id,
+			error: null
+		}
+		
+		await guild.roles.cache.get(id)?.edit({ name: roles[id] })
+			.catch(err => {
+				status.error = err
+			})
+
+		roleLoadStatuses.push(status)
 	}
+
+	return {channels: channelLoadStatuses, roles: roleLoadStatuses}
 }
 
 module.exports = loadServerState
